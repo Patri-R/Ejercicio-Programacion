3d00e2acc93d1824be7a0de4eb875502381146e8
Patri-R <patricia.rodriguez@campusfp.es>
2020-04-01 09:30:57 +0200

    Creacion de variables

--- Main/Ejercicio Programacion/01.Main.cs
+++ Main/Ejercicio Programacion/01.Main.cs
@@ -1,1 +1,29 @@
 ﻿//Patri
+
+public class Program
+{
+    struct cancion
+    {
+        public string titulo;
+        public string artista;
+        public string duracion;
+        public byte tamanoKB;
+        
+    }
+    public int cantidadFicheros = 0;
+
+
+
+    
+
+    static byte opcion = 0;
+    static int capacidad = 1000;
+    static cancion[] canciones = new cancion[capacidad];
+
+
+
+    public static void Main()
+    {
+
+    }
+}
