e77a6a9de35bb3e0be4d27366c1e417015087b6c
Wyllink <guillermoqnk@gmail.com>
2020-04-15 10:03:05 +0200

    Update 03.AnadirDato.cs

--- Main/Ejercicio Programacion/03.AnadirDato.cs
+++ Main/Ejercicio Programacion/03.AnadirDato.cs
@@ -13,13 +13,13 @@ namespace Ejercicio_Programacion
             cantidadFicheros ++;
             Console.WriteLine("A continuación te mostraré los datos a rellenar" +
                 "\n-Título:");
-            program.Canciones[CantidadFicheros].titulo = Console.ReadLine();
+            program.Canciones[program.CantidadFicheros].titulo = Console.ReadLine();
             Console.WriteLine("-Artista:");
-            program.Canciones[CantidadFicheros].artista = Console.ReadLine();
+            program.Canciones[program.CantidadFicheros].artista = Console.ReadLine();
             Console.WriteLine("-Duración:");
-            program.Canciones[CantidadFicheros].duracion = Console.ReadLine();
+            program.Canciones[program.CantidadFicheros].duracion = Console.ReadLine();
             Console.WriteLine("-Tamaño (Kb):");
-            program.Canciones[CantidadFicheros].tamañoKB = Console.ReadLine();
+            program.Canciones[program.CantidadFicheros].tamañoKB = Console.ReadLine();
         }
 
     }
