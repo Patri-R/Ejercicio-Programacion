0514b1136ef84268b527f66866a165dd357c6dba
Wyllink <guillermoqnk@gmail.com>
2020-04-15 09:41:10 +0200

    Update 03.AnadirDato.cs

--- Main/Ejercicio Programacion/03.AnadirDato.cs
+++ Main/Ejercicio Programacion/03.AnadirDato.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Ejercicio_Programacion
 {
-    class AnyadirDato
+    public class AnyadirDato
     {
+        
+        public void saludo()
+        {
+            cantidadFicheros ++;
+            Console.WriteLine("A continuación te mostraré los datos a rellenar" +
+                "\n-Título:");
+            canciones[cantidadFicheros].titulo = Console.ReadLine();
+            Console.WriteLine("-Artista:");
+            canciones[cantidadFicheros].artista = Console.ReadLine();
+            Console.WriteLine("-Duración:");
+            canciones[cantidadFicheros].duracion = Console.ReadLine();
+            Console.WriteLine("-Tamaño (Kb):");
+            canciones[cantidadFicheros].tamañoKB = Console.ReadLine();
+        }
+
     }
 }
