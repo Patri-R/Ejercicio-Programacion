f8dd692aeb8c9303b0aaeb6db6aecc994ec9c3cd
JavierMiron <jamibla9@gmail.com>
2020-04-22 09:31:25 +0200

    menu y buscar tamaño

--- Main/Ejercicio Programacion/02.Menu.cs
+++ Main/Ejercicio Programacion/02.Menu.cs
@@ -6,5 +6,60 @@ namespace Ejercicio_Programacion
 {
     class Menu
     {
+        int opcion = 0;
+
+        public void Menu()
+        {
+            Console.WriteLine("Hola, bienvenido al menu del programa");
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("Hola de nuevo si quieres acceder a diferentes lugares del menu tendras");
+                Console.WriteLine("Pulsa 1 si quieres añadir un dato al programa");
+                Console.WriteLine("Pulsa 2 si quieres mostrar todos los datos");
+                Console.WriteLine("Pulse 3 si quieres buscar archivos por su tamanyo");
+                Console.WriteLine("Pulse 4 si quieres buscar archivos por su letra");
+                Console.WriteLine();
+                Console.WriteLine("Introduzca el numero que corresponda a lo que desea");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Bienvenido al caso 1, aqui se añadirán datos al programa");
+                            Console.WriteLine();
+                            AnadirDato();
+                        }
+
+                    case 2:
+                        {
+                            Console.WriteLine("Bienvenido al caso 2 aqui te mostraremos todos los datos del programa");
+                            Console.WriteLine();
+                            MostrarDatos();
+                        }
+
+                    case 3:
+                        {
+                            Console.WriteLine("Bienvenido al caso 3, aqui se buscaran los archivos por su tamaño");
+                            Console.WriteLine();
+                            BuscarPorTamano();
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine("Bienvenido al caso 4, aqui se buscaran archivos por su letra");
+                            Console.WriteLine();
+                            BusquedaPorLetra();
+                        }
+                }
+
+            } while ((opcion > 4) || (opcion < 1));
+
+            Console.ReadLine();
+
+        }
     }
 }
--- Main/Ejercicio Programacion/05.BuscarPorTamano.cs
+++ Main/Ejercicio Programacion/05.BuscarPorTamano.cs
@@ -6,5 +6,28 @@ namespace Ejercicio_Programacion
 {
     class BuscarPorTamano
     {
+        int aciertos = 0;
+
+        public void BuscarPorTamano()
+        {
+            program program = new program();
+
+            Console.WriteLine("Bienvenido a la busqueda del tamayo, introduzca por favor a partir de que tamanyo quieres buscar (en kb)");
+            Console.WriteLine();
+            short busqueda = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < program.NumeroDeFicheros; i++)
+            {
+                if (program.canciones[i].tamanyo >= busqueda)
+                {
+                    aciertos++;
+                    Console.WriteLine();
+                    Console.WriteLine("Titulo {0}:");
+                    Console.WriteLine("Autor {1}:");
+                    Console.WriteLine("Duracion {2}:");
+                    Console.WriteLine("Tamanyo {3}:");                  
+                }
+            }
+        }
     }
 }
