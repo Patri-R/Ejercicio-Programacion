969da756e9d42b1b5e24816e62aa3c8b18564194
Patri-R <patricia.rodriguez@campusfp.es>
2020-04-22 10:06:13 +0200

    Compatibilización de versiones. Programa final

--- Main/Ejercicio Programacion/05.BuscarPorTamano.cs
+++ Main/Ejercicio Programacion/05.BuscarPorTamano.cs
@@ -5,25 +5,22 @@ public class BusquedaTamano
     {
         int aciertos = 0;
 
-        public void BuscarPorTamano()
+        Program program = new Program();
+
+        Console.WriteLine("Bienvenido a la busqueda del tamayo, introduzca por favor a partir de que tamanyo quieres buscar (en kb)");
+        Console.WriteLine();
+        short busqueda = Convert.ToInt16(Console.ReadLine());
+
+        for (int i = 0; i < program.CantidadFicheros; i++)
         {
-            program program = new program();
-
-            Console.WriteLine("Bienvenido a la busqueda del tamayo, introduzca por favor a partir de que tamanyo quieres buscar (en kb)");
-            Console.WriteLine();
-            short busqueda = Convert.ToInt16(Console.ReadLine());
-
-            for (int i = 0; i < program.NumeroDeFicheros; i++)
+            if (program.Canciones[i].tamanoKB >= busqueda)
             {
-                if (program.canciones[i].tamanyo >= busqueda)
-                {
-                    aciertos++;
-                    Console.WriteLine();
-                    Console.WriteLine("Titulo {0}:");
-                    Console.WriteLine("Autor {1}:");
-                    Console.WriteLine("Duracion {2}:");
-                    Console.WriteLine("Tamanyo {3}:");                  
-                }
+                aciertos++;
+                Console.WriteLine();
+                Console.WriteLine("Titulo {0}:", program.Canciones[i].titulo);
+                Console.WriteLine("Autor {1}:", program.Canciones[i].artista);
+                Console.WriteLine("Duracion {2}:", program.Canciones[i].duracion);
+                Console.WriteLine("Tamanyo {3}:", program.Canciones[i].tamanoKB);
             }
         }
     }
